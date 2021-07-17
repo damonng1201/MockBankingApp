6bb53f0109e287ff5d0d69ef531016811cf170bb
damon1201 <damonng85@gmail.com>
2021-07-17 11:42:22 +0800

    Initial Commit

---
+++ MockBankingApp/MockBankingAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  MockBankingAppApp.swift
+//  MockBankingApp
+//
+//  Created by Damon on 17/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MockBankingAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
